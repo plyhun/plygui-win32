4b2d0c8a93721a6fc25a6e28587eba4e0a36caee
Serhii Plyhun <snuk188@gmail.com>
2018-09-14 13:43:25 +0200

    Prelude mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-#![cfg(target_os="windows")]
+#![cfg(target_os = "windows")]
 #![windows_subsystem = "windows"]
-
 #![cfg_attr(feature = "cargo-clippy", allow(cast_lossless))]
 #![cfg_attr(feature = "cargo-clippy", allow(too_many_arguments))]
 #![cfg_attr(feature = "cargo-clippy", allow(type_complexity))]
@@ -17,34 +16,34 @@ extern crate winapi;
 pub mod common;
 
 mod application;
-mod window;
 mod button;
-mod layout_linear;
 mod frame;
+mod layout_linear;
 mod splitted;
-
-pub use self::application::Application;
-pub use self::window::Window;
-pub use self::button::Button;
-pub use self::layout_linear::LinearLayout;
-pub use self::frame::Frame;
-pub use self::splitted::Splitted;
+mod window;
 
 #[cfg(feature = "markup")]
 pub fn register_members(registry: &mut plygui_api::markup::MarkupRegistry) {
-    registry
-        .register_member(plygui_api::markup::MEMBER_TYPE_BUTTON.into(), button::spawn)
-        .unwrap();
-    registry
-        .register_member(plygui_api::markup::MEMBER_TYPE_LINEAR_LAYOUT.into(),
-                         layout_linear::spawn)
-        .unwrap();
-    registry
-        .register_member(plygui_api::markup::MEMBER_TYPE_FRAME.into(),
-                         frame::spawn)
-        .unwrap(); 
-    registry
-        .register_member(plygui_api::markup::MEMBER_TYPE_SPLITTED.into(),
-                         splitted::spawn)
-        .unwrap();       
+    registry.register_member(plygui_api::markup::MEMBER_TYPE_BUTTON.into(), button::spawn).unwrap();
+    registry.register_member(plygui_api::markup::MEMBER_TYPE_LINEAR_LAYOUT.into(), layout_linear::spawn).unwrap();
+    registry.register_member(plygui_api::markup::MEMBER_TYPE_FRAME.into(), frame::spawn).unwrap();
+    //registry.register_member(plygui_api::markup::MEMBER_TYPE_SPLITTED.into(), splitted::spawn).unwrap();
+}
+
+pub mod prelude {
+    pub use plygui_api::callbacks;
+    pub use plygui_api::controls::*;
+    pub use plygui_api::ids::*;
+    pub use plygui_api::layout;
+    pub use plygui_api::types::*;
+    pub use plygui_api::utils;
+
+    pub mod imp {
+        pub use application::Application;
+        pub use button::Button;
+        pub use frame::Frame;
+        pub use layout_linear::LinearLayout;
+        pub use splitted::Splitted;
+        pub use window::Window;
+    }
 }
